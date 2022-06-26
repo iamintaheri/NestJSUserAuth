79956a27fd624e1dcf0e8067d47ef5dc1e857ea3
Amin Taheri <m_amin@icloud.com>
2022-06-26 20:48:32 +0200

    Implement: JWT strategy exported

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { JwtStrategy } from './jwt.strategy';
 import { SmsHelper } from './sms.helper';
 
 @Module({
-  exports: [SmsHelper],
+  exports: [SmsHelper, JwtStrategy],
 })
 export class AuthModule {}
