f1bbe7620332b5ac84ffe3b45c9e8955cf7d4540
Amin Taheri <m_amin@icloud.com>
2022-06-26 21:34:57 +0200

    Add: Mikro-ORM for auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { SmsHelper } from './sms.helper';
+import { PassportModule } from '@nestjs/passport';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import * as config from 'config';
+import { AuthController } from './auth.controller';
 
+const jwtConf = config.get('jwt');
 @Module({
+  imports: [
+    MikroOrmModule.forFeature({ entities: [User] }),
+    JwtModule.register({
+      secret: jwtConf.secret,
+      signOptions: {
+        expiresIn: jwtConf.expiresIn,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService, SmsHelper, JwtStrategy],
   exports: [SmsHelper, JwtStrategy],
 })
