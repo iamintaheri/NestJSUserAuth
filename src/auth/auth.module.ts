0dfd0a2a74f8b183271d7ffb210bfabc3a9ee0c6
Amin Taheri <m_amin@icloud.com>
2022-06-26 21:17:48 +0200

    Add: auth service to the authModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { SmsHelper } from './sms.helper';
 
 @Module({
+  providers: [AuthService, SmsHelper, JwtStrategy],
   exports: [SmsHelper, JwtStrategy],
 })
 export class AuthModule {}
