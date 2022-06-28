2a5b6fa8a3a4502a5ec114f62e5e7daebf064370
Amin Taheri <m_amin@icloud.com>
2022-06-28 21:19:14 +0200

    Add: Mikro-ORM for root of the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import * as config from 'config';
 
+const dbConfig = config.get('db');
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MikroOrmModule.forRoot({
+      type: process.env.DB_TYPE || dbConfig.type,
+      clientUrl: process.env.DB_URL || dbConfig.url,
+      autoLoadEntities: true,
+      debug: process.env.DB_DEBUG || dbConfig.debug,
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
