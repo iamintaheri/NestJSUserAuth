4e45319c4cbec3b204d2229f3c7b7ad061cdc7b5
Amin Taheri <m_amin@icloud.com>
2022-06-29 22:36:01 +0200

    Add: ensure indexes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import * as config from 'config';
+import { User } from './auth/user.entity';
 
 const dbConfig = config.get('db');
 @Module({
   imports: [
     MikroOrmModule.forRoot({
+      //entities: [User],
       type: process.env.DB_TYPE || dbConfig.type,
       clientUrl: process.env.DB_URL || dbConfig.url,
       autoLoadEntities: true,
+      ensureIndexes: true,
       debug: process.env.DB_DEBUG || dbConfig.debug,
     }),
     AuthModule,
